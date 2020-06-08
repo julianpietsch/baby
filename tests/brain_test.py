e6e69600b244d22f76a562eb8a5118173888dd97
Julian Pietsch <julian.pietsch@ed.ac.uk>
2020-06-08 17:26:24 +0100

    get tests working with GPU

--- tests/brain_test.py
+++ tests/brain_test.py
@@ -26,12 +26,14 @@ def test_modelsets(modelsets):
         # Make sure all model files exist
         for k, v in mset.items():
             if k.endswith('_file'):
-                assert (MODEL_PATH / v).is_file())
+                assert (MODEL_PATH / v).is_file()
 
     # Ensure that the default test model is present
     assert DEFAULT_MODELSET in modelsets
 
 
-def test_init(modelsets):
+def test_init(modelsets, tf_session_graph):
     # Attempt to load default evolve model
-    BabyBrain(**modelsets[DEFAULT_MODELSET])
+    tf_session, tf_graph = tf_session_graph
+    BabyBrain(session=tf_session, graph=tf_graph,
+              **modelsets[DEFAULT_MODELSET])
--- tests/conftest.py
+++ tests/conftest.py
@@ -36,10 +36,43 @@ def modelsets():
 
 
 @pytest.fixture(scope='session')
-def bb_evolve60(modelsets):
-    return BabyBrain(**modelsets['evolve_brightfield_60x_5z'])
+def tf_session_graph():
+    import tensorflow as tf
+
+    # Compensate for bug in tensorflow + RTX series NVidia GPUs
+    tf_version = tuple(int(v) for v in tf.version.VERSION.split('.'))
+    if tf_version[0] == 1:
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        tf_session = tf.Session(config=config)
+        tf_graph = tf.get_default_graph()
+    elif tf_version[0] == 2:
+        tf_session, tf_graph = 2 * (None,)
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus),
+                    "Logical GPUs")
+    else:
+        raise Exception(
+            'Unsupported version of tensorflow encountered ({})'.format(
+                tf.version.VERSION
+            )
+        )
+
+    return tf_session, tf_graph
+
+@pytest.fixture(scope='session')
+def bb_evolve60(modelsets, tf_session_graph):
+    tf_session, tf_graph = tf_session_graph
+    return BabyBrain(session=tf_session, graph=tf_graph,
+                     **modelsets['evolve_brightfield_60x_5z'])
 
 
 @pytest.fixture(scope='module')
-def bb_prime60(modelsets):
-    return BabyBrain(**modelsets['prime95b_brightfield_60x_5z'])
+def bb_prime60(modelsets, tf_session_graph):
+    tf_session, tf_graph = tf_session_graph
+    return BabyBrain(session=tf_session, graph=tf_graph,
+                     **modelsets['prime95b_brightfield_60x_5z'])
