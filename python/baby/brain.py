3b993c6c85ad298db101ce8a4c27cfbfa4aa377b
Julian Pietsch <julian.pietsch@ed.ac.uk>
2020-06-18 11:56:29 +0100

    update default ctrack model in BabyBrain

--- python/baby/brain.py
+++ python/baby/brain.py
@@ -83,7 +83,7 @@ class BabyBrain(object):
 
         if celltrack_model_file is None:
             celltrack_model_file = join(models_path,
-                                        'ctrack_randomforest_20200325.pkl')
+                                        'ctrack_randomforest_20200513.pkl')
         elif not isfile(celltrack_model_file):
             celltrack_model_file = join(models_path, celltrack_model_file)
 
