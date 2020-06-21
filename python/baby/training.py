13ae7320d8d85e5b312cb37a04e2b58d946cb15a
alan <afer.mg@gmail.com>
2020-06-21 17:27:03 +0100

    Cleaned module calls

--- python/baby/training.py
+++ python/baby/training.py
@@ -19,9 +19,6 @@ from .generator import ImageLabel
 from .losses import bce_dice_loss, dice_coeff
 from . import models
 
-from imageio import imread
-import pandas as pd
-
 custom_objects = {'bce_dice_loss': bce_dice_loss, 'dice_coeff': dice_coeff}
 
 
