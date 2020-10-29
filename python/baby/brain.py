021126156fc150722f4758ae525abca41aea81f6
Alan Munoz <afer.mg@gmail.com>
2020-10-29 23:04:05 +0000

    add new model to bbrain

--- python/baby/brain.py
+++ python/baby/brain.py
@@ -95,7 +95,7 @@ class BabyBrain(object):
 
         if celltrack_model_file is None:
             celltrack_model_file = join(models_path,
-                                        'ctrack_randomforest_20201012.pkl')
+                                        'ct_rf_20201029_7.pkl')
         elif not isfile(celltrack_model_file):
             celltrack_model_file = join(models_path, celltrack_model_file)
 
